245b353fe1fde9d4627e06d2d21cbc0d1ea79718
Damir Basic Knezevic <damirbasicknezevic@gmail.com>
2018-10-09 19:28:07 +0200

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,5 @@
+﻿:: Run in Command Prompt (cmd.exe)
+:: This script will install both the Chocolately .exe file and add the
+:: choco command to your PATH variable﻿﻿
+
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
